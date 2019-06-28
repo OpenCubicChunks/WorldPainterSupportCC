fc42c206010c4122b7bb3f93b09245b129907450
Barteks2x <Barteks2x@gmail.com>
2019-06-28 22:10:46 +0200

    Revert platform ID.
    Fix for https://github.com/Captain-Chaos/WorldPainter/issues/125

--- src/main/java/io/github/opencubicchunks/worldpainterplugin/CubicChunksPlatformProvider.java
+++ src/main/java/io/github/opencubicchunks/worldpainterplugin/CubicChunksPlatformProvider.java
@@ -182,8 +182,8 @@ public class CubicChunksPlatformProvider extends AbstractPlugin implements Block
     }
 
     static final Platform CUBICCHUNKS = new Platform(
-        "io.github.oopencubicchunks.cubicchunks",
-        "CubicChunks",
+        "org.pepsoft.cubicchunks",
+        "Cubic Chunks (1.10.2 - 1.12.2)",
         256,
         Math.min(Constants.MAX_HEIGHT, Integer.MAX_VALUE / 2),
         Math.min(Constants.MAX_HEIGHT, Integer.MAX_VALUE / 2),
